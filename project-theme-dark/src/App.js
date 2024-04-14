6f4537fa09cbc676f9eff80797f4e096819fa04d
guilherme <guicarvalhooliveira2468@gmail.com>
2024-04-14 14:45:45 +0100

    darkmode

--- project-theme-dark/src/App.js
+++ project-theme-dark/src/App.js
@@ -9,16 +9,9 @@ function App() {
         <DarkMode />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Copyright © 2024 Guilherme Carvalho. All Rights Reserved.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
     </div>
   );
